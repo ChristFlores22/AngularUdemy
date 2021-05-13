af209552041b7c0abd95fc20a3ef9c0824102a03
Christian Flores <cfroot@P09CF61089LPT01.credito.bcp.com.pe>
2021-05-13 09:57:15 -0500

    Angular Udemy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    //Se listan los componentes que se utilizaran en la aplicación.
+    AppComponent,
+    ContadorComponent,
+    HeroeComponent
   ],
   imports: [
+    // Se listan los modulos a utilizar en la aplicación.
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],//Servicios especificos al Modulo.
+  bootstrap: [AppComponent] // Modulo Principal
 })
 export class AppModule { }
