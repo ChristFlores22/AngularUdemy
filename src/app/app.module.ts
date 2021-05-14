200b56384856a5f5b73eb745d83447df72acbb87
Christian Flores <cfroot@P09CF61089LPT01.credito.bcp.com.pe>
2021-05-14 18:15:13 -0500

    Segun subida capitulo 43

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { ListadoComponent } from './heroes/listado/listado.component';
 
 @NgModule({
   declarations: [
     //Se listan los componentes que se utilizaran en la aplicación.
     AppComponent,
     ContadorComponent,
-    HeroeComponent
+    HeroeComponent,
+    ListadoComponent
   ],
   imports: [
     // Se listan los modulos a utilizar en la aplicación.
